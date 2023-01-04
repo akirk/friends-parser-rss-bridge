094734c0fb9812a1ca5f0f5d6049b03bac28af32
Alex Kirk <akirk@users.noreply.github.com>
2023-01-04 09:58:15 +0100

    Update feed item

--- class-feed-item.php
+++ class-feed-item.php
@@ -13,4 +13,27 @@ namespace Friends;
 /**
  * This class describes a friends feed item.
  */
-class Feed_Item {}
+class Feed_Item {
+	public $title;
+	public $permalink;
+	public $gravatar;
+	public $post_title;
+	public $author;
+	public $post_content;
+	public $content;
+	public $comment_count;
+	public $comments_feed;
+	public $post_id;
+	public $post_format;
+	public $post_status;
+	public $date;
+	public $_feed_rule_delete;
+	public $_feed_rule_transform;
+	public $_is_new;
+	public $_full_content_fetched;
+	public function __construct( $data = array() ) {
+		foreach ( $data as $key => $value ) {
+			$this->$key = $value;
+		}
+	}
+}
